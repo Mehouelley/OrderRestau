236ae1f61189162250124a708c1d454cc4aee397
DevAfrica2024 <fabrice.mehouelley@africadigitalizer.com>
2026-05-22 17:35:14 +0100

    feat: update root endpoint to return JSON status

--- orderresto-backend/routes/web.php
+++ orderresto-backend/routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'orderresto-backend',
+        'message' => 'API Laravel is running',
+    ]);
 });
 
